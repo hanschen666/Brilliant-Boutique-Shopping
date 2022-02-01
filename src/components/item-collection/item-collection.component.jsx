fa9537e60629f6507e7dc189c31fe5a6cc58947d
hanschen666 <15814841133@163.com>
2022-02-01 23:46:47 +0800

    additem if already log in

--- src/components/item-collection/item-collection.component.jsx
+++ src/components/item-collection/item-collection.component.jsx
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 import "./item-collection.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import { AddItem } from "../../redux/cart/cart.action";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-const ItemCollection = ({ item, AddItem }) => {
+const ItemCollection = ({ item, AddItem, currentUser }) => {
   const { name, price, imageUrl } = item;
   return (
     <div className="item-collection">
@@ -16,7 +17,14 @@ const ItemCollection = ({ item, AddItem }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <CustomButton onClick={() => AddItem(item)} inverted>
+      <CustomButton
+        onClick={() => {
+          currentUser !== null
+            ? AddItem(item)
+            : alert("Please log into your account!");
+        }}
+        inverted
+      >
         Add to cart
       </CustomButton>
     </div>
@@ -26,4 +34,9 @@ const ItemCollection = ({ item, AddItem }) => {
 const mapDispatchToProps = (dispatch) => ({
   AddItem: (item) => dispatch(AddItem(item)),
 });
-export default connect(null, mapDispatchToProps)(ItemCollection);
+
+const mapStateToProps = (state) => ({
+  currentUser: selectCurrentUser(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemCollection);
